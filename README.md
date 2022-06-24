a1a78ddf87d175cd1b9098cbdf69dea55b390b27
Chinonyerem Obioha <chinoye63@gmail.com>
2022-06-24 21:58:14 +0100

    zuri bio task

---
+++ README.md
@@ -0,0 +1,1 @@
+# Zuri5BioTask
---
+++ script.js
@@ -0,0 +1,6 @@
+let name = "Chinonyerem Obioha";
+let height = "185cm";
+let country = "Nigeria";
+document.getElementById("demo").innerHTML= "My name is: "+ name;
+document.getElementById("demo2").innerHTML= "My height is: "+ height;
+document.getElementById("demo3").innerHTML= "I am from: "+ country;
